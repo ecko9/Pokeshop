9e25d512c21f4d561677773b83306aaf8429da62
ecko9 <jean.ferstler@gmail.com>
2022-03-02 04:59:41 +0100

    add assets bg + create home (welcome) page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='Home'>
+
+      <div className='bg-overlay' />
+
+      <div className='welcome'>
+        <h1>PokéShoP</h1>
+        <h2>Achetez les tous!</h2>
+        <div className='btn-enter' onClick={e => navigate(`/pokemons`)}>
+          {"Entrer"}
+        </div>
+      </div>
 
     </div>
   );
